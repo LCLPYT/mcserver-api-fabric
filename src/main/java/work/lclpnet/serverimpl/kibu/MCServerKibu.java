5dddd4588bc1291d1855172ca9c75ec75a8e3628
LCLP <lukas@lclpnet.work>
2023-06-29 16:44:30 +0200

    convert to kibu plugin

---
+++ src/main/java/work/lclpnet/serverimpl/kibu/MCServerKibu.java
@@ -0,0 +1,11 @@
+package work.lclpnet.serverimpl.kibu;
+
+import work.lclpnet.kibu.plugin.KibuPlugin;
+
+public class MCServerKibu extends KibuPlugin {
+
+    @Override
+    protected void loadKibuPlugin() {
+
+    }
+}
