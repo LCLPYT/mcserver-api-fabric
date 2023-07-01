f5b47b2e7397e53f4ccd8cd7ac5d3ea2c122a7dd
LCLP <lukas@lclpnet.work>
2023-07-01 20:19:20 +0200

    add KibuPlatformBridge

---
+++ src/main/java/work/lclpnet/serverimpl/kibu/util/KibuMCMessageImpl.java
@@ -0,0 +1,92 @@
+package work.lclpnet.serverimpl.kibu.util;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.text.TextColor;
+import net.minecraft.util.math.MathHelper;
+import work.lclpnet.serverapi.msg.MCMessage;
+
+import java.util.List;
+
+public class KibuMCMessageImpl {
+
+    private final KibuServerTranslation translations;
+
+    protected KibuMCMessageImpl(KibuServerTranslation translations) {
+        this.translations = translations;
+    }
+
+    public Text convert(MCMessage msg, ServerPlayerEntity receiver) {
+        MutableText root = Text.empty();
+
+        append(root, msg, null, receiver);
+
+        return root;
+    }
+
+    private void append(MutableText parent, MCMessage msg, MCMessage.MessageStyle parentStyle, ServerPlayerEntity receiver) {
+        MCMessage.MessageStyle style = switch (msg.getColorMode()) {
+            case INHERIT -> parentStyle != null ? parentStyle : msg.getStyle();
+            case LOCAL -> msg.getStyle();
+            default -> throw new IllegalStateException("Color mode not implemented: " + msg.getColorMode());
+        };
+
+        if (!msg.isTextNode()) {
+            for (MCMessage child : msg.getChildren()) {
+                append(parent, child, style, receiver);
+            }
+
+            return;
+        }
+
+        final String content;
+
+        if (msg instanceof MCMessage.MCTranslationMessage translationMsg) {
+            List<MCMessage> substituteList = translationMsg.getSubstitutes();
+            String[] substitutes = new String[substituteList.size()];
+
+            for (int i = 0; i < substituteList.size(); i++) {
+                MCMessage subMsg = substituteList.get(i);
+                substitutes[i] = subMsg.getText();  // TODO replace with convert(subMsg) and replace accordingly
+            }
+
+            content = translations.translate(receiver, translationMsg.getText(), (Object[]) substitutes);
+        } else {
+            content = msg.getText();
+        }
+
+        Style mcStyle = convert(style);
+
+        MutableText text = Text.literal(content).setStyle(mcStyle);
+
+        parent.append(text);
+    }
+
+    private Style convert(MCMessage.MessageStyle messageStyle) {
+        Style style = Style.EMPTY;
+
+        if (messageStyle.isReset()) {
+            return style;
+        }
+
+        MCMessage.MessageColor color = messageStyle.getColor();
+        if (color != null) {
+            int rgb = 0x000000;
+            rgb |= MathHelper.clamp(color.red, 0, 255) << 4;
+            rgb |= MathHelper.clamp(color.green, 0, 255) << 2;
+            rgb |= MathHelper.clamp(color.blue, 0, 255);
+
+            style = style.withColor(TextColor.fromRgb(rgb));
+        }
+
+        if (messageStyle.isBold()) style = style.withBold(true);
+        if (messageStyle.isItalic()) style = style.withItalic(true);
+        if (messageStyle.isObfuscated()) style = style.withObfuscated(true);
+        if (messageStyle.isStrikethrough()) style = style.withStrikethrough(true);
+        if (messageStyle.isUnderline()) style = style.withUnderline(true);
+
+        return style;
+    }
+}
---
+++ src/main/java/work/lclpnet/serverimpl/kibu/util/KibuPlatformBridge.java
@@ -0,0 +1,62 @@
+package work.lclpnet.serverimpl.kibu.util;
+
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import org.slf4j.Logger;
+import work.lclpnet.serverapi.msg.MCMessage;
+import work.lclpnet.serverapi.util.IPlatformBridge;
+import work.lclpnet.serverapi.util.MojangAPI;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+public class KibuPlatformBridge implements IPlatformBridge {
+
+    private final PlayerManager playerManager;
+    private final Logger logger;
+    private final KibuMCMessageImpl messageSerializer;
+
+    public KibuPlatformBridge(PlayerManager playerManager, KibuServerTranslation translations, Logger logger) {
+        this.playerManager = playerManager;
+        this.logger = logger;
+        this.messageSerializer = new KibuMCMessageImpl(translations);
+    }
+
+    @Override
+    public void sendMessageTo(String playerUuid, MCMessage msg) {
+        UUID uuid = UUID.fromString(playerUuid);
+        ServerPlayerEntity player = playerManager.getPlayer(uuid);
+
+        if (player == null) {
+            logger.error("There is no player with UUID '{}' online!", playerUuid);
+            return;
+        }
+
+        Text message = messageSerializer.convert(msg, player);
+
+        player.sendMessage(message);
+    }
+
+    @Override
+    public CompletableFuture<String> getPlayerNameByUUID(String uuid) {
+        ServerPlayerEntity online = playerManager.getPlayer(uuid);
+
+        if (online != null) {
+            return CompletableFuture.completedFuture(online.getEntityName());
+        }
+
+        return MojangAPI.getUsernameByUUID(uuid);
+    }
+
+    @Override
+    public CompletableFuture<String> getPlayerUUIDByName(String name) {
+        ServerPlayerEntity online = playerManager.getPlayer(name);
+
+        if (online != null) {
+            return CompletableFuture.completedFuture(online.getUuid().toString());
+        }
+
+        return MojangAPI.getUUIDByUsername(name);
+    }
+}
